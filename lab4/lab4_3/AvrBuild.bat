dfecc8fc6c3628ff7fcaf75bbb540a76e24ff840
Alexander Bondarev <bondarev-al@mail.ru>
2021-05-07 06:07:46 +0300

    Начало lab4_3

---
+++ lab4/lab4_3/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_3\labels.tmp" -fI -W+ie -C V2E -o "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_3\lab4_3.hex" -d "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_3\lab4_3.obj" -e "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_3\lab4_3.eep" -m "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_3\lab4_3.map" "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_3\lab4_3.asm"
