7be3893d673efd3d6acfe21c1362b7bf40c7d327
Alexander Bondarev <bondarev-al@mail.ru>
2021-05-07 08:16:46 +0300

    Код пятого пункта

---
+++ lab4/lab4_5/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_5\labels.tmp" -fI -W+ie -C V2E -o "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_5\lab4_5.hex" -d "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_5\lab4_5.obj" -e "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_5\lab4_5.eep" -m "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_5\lab4_5.map" "E:\BMSTU\6_Microprocessors\labs\lab4\lab4_5\lab4_5.asm"
